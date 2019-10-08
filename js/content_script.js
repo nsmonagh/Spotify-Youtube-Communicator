0efcb206bcd62ac908778c7975b6e8e268ae55ce
Noah <noah.s.monaghan@gmail.com>
2019-10-08 11:53:27 -0500

    Fixed whitespace

--- js/content_script.js
+++ js/content_script.js
@@ -18,8 +18,8 @@ function initializeExtension() {
     "https://thodogantu.pythonanywhere.com/auth",
     function (data) {
       spotifyApi.setAccessToken(data);
-	  // Call function to get URL
-	  getSpotifyURL();
+      // Call function to get URL
+      getSpotifyURL();
     }
   );
 }
@@ -29,10 +29,10 @@ function getSpotifyURL() {
   // Gets the id of the current video
   let video_id = window.location.search.split('v=')[1];
   if (typeof video_id == 'undefined')
-	  return;
+    return;
   let ampersandPosition = video_id.indexOf('&');
   if (ampersandPosition != -1) {
-	video_id = video_id.substring(0, ampersandPosition);
+    video_id = video_id.substring(0, ampersandPosition);
   }
 
   // Get song and artist
@@ -40,9 +40,9 @@ function getSpotifyURL() {
     "https://thodogantu.pythonanywhere.com/access/" + video_id,
     function (data) {
       let result = data.split(' - ');
-	  
-	  if (result[0] == 'NullArtist')
-		  return;
+
+      if (result[0] == 'NullArtist')
+        return;
 
       // Get URL
       spotifyApi.searchTracks(result[1] + ' artist:"' + result[0] + '"')
@@ -51,11 +51,10 @@ function getSpotifyURL() {
           button.src = chrome.extension.getURL('images/spotify32.png');
           $('#6481696ad23546e193ce05408a344d7c').off('click').on('click', function () {
             try {
-			  window.open(data.tracks.items[0].external_urls.spotify);
-			}
-			catch (error) {
-			  button.src = chrome.extension.getURL('images/gray_spotify32.png');
-			}
+              window.open(data.tracks.items[0].external_urls.spotify);
+            } catch (error) {
+              button.src = chrome.extension.getURL('images/gray_spotify32.png');
+            }
           })
         });
     }
@@ -63,7 +62,7 @@ function getSpotifyURL() {
 }
 
 // Update button based on page change
-let oldURL= document.URL;
+let oldURL = document.URL;
 
 // window.setInterval takes a function and executes it after 
 // a given time (defined by the second parmeter)in miliseconds
@@ -73,10 +72,10 @@ let urlChangeHandler = window.setInterval(checkURLChange, 500);
 function checkURLChange() {
   newURL = document.URL;
   if (newURL !== oldURL) {
-   let button = document.getElementById('6481696ad23546e193ce05408a344d7c');
-   button.src = chrome.extension.getURL('images/gray_spotify32.png');
-   $('#6481696ad23546e193ce05408a344d7c').off('click');
-   getSpotifyURL();
-   oldURL = newURL;
+    let button = document.getElementById('6481696ad23546e193ce05408a344d7c');
+    button.src = chrome.extension.getURL('images/gray_spotify32.png');
+    $('#6481696ad23546e193ce05408a344d7c').off('click');
+    getSpotifyURL();
+    oldURL = newURL;
   }
 }
